2469461c07bb2ba819820536c4d75eb6c61199fb
Yunyi Tang <yytang@eduroam-104.eduroam.yorku.ca>
2015-09-26 18:49:17 -0400

    model

--- users/models.py
+++ users/models.py
@@ -4,10 +4,17 @@ from djangotoolbox.fields import EmbeddedModelField, ListField
 
 class StandardUser(User):
 	name = models.CharField(max_length=20)
+	localtion
 
 class Questionnaire(models.Model):
 	questions = ListField(EmbeddedModelField('Question'))
 
 class Question(models.Model):
+	question = models.CharField(max_length=255)
+
+class QuestionAnswers(models.Model):
+	question = EmbeddedModelField('Question')
+	answer = models.IntegerField()
+	user = EmbeddedModelField('User')
 
 
