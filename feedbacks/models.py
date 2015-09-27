6afca404e6f216c06d02157f2791ae8f3f0cf47f
Sherry Wong <sherrybywong@Sherrys-MacBook-Pro.local>
2015-09-26 20:33:33 -0400

    locations and feedback

--- feedbacks/models.py
+++ feedbacks/models.py
@@ -1,20 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
 from djangotoolbox.fields import EmbeddedModelField, ListField
+from users.models import StandardUser
+from events.models import Events
 
-class StandardUser(User):
-	name = models.CharField(max_length=20)
-	localtion
-
-class Questionnaire(models.Model):
-	questions = ListField(EmbeddedModelField('Question'))
-
-class Question(models.Model):
-	question = models.CharField(max_length=255)
-
-class QuestionAnswers(models.Model):
-	question = EmbeddedModelField('Question')
-	answer = models.IntegerField()
-	user = EmbeddedModelField('User')
-
-
+class Feedback(models.Model):
+	event = EmbeddedModelField('Event');
+	reviewer = EmbeddedModelField('StandardUser');
+	receiver = EmbeddedModelField('StandardUser');
+	score = models.IntegerField();
